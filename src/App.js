b0f9c98e1cc70c3aecb8a08a0c395ec716fd3732
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-02-20 13:17:07 -0600

    add face detection box

--- src/App.js
+++ src/App.js
@@ -8,20 +8,41 @@ import Logo from './components/Logo/Logo.js';
 import Rank from './components/Rank/Rank.js';
 import './App.css';
 
-const PAT = 'adfdae0e0d864b6498612d5b95f87c65';
+const PAT = '64a6c3e0d1b749048656fbeb71f289a6';
 const USER_ID = 'superjamey';       
 const APP_ID = 'my-first-application';
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-const IMAGE_URL = 'https://dynamicmedia.accenture.com/is/image/accenture/accenture-women-who-drive-change-featured-400x400?qlt=85&ts=1675710993260&$auto-jpg$&dpr=off';
+//const imageUrl = 'https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             input: '',
-            imageUrl: ''
+            imageUrl: '',
+            box: {},
         } 
+    }
+    
+    calculateFaceLocation = (resp) => {
+        console.log(resp);
+        const clarifaiFace = resp;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        console.log(image.width);
+        console.log(image.height);
+        return {
+            left_col: (clarifaiFace.left_col) * width,
+            top_row: clarifaiFace.top_row * height,
+            right_col: width - (clarifaiFace.right_col * width),
+            bottom_row: height - (clarifaiFace.bottom_row * height)
+        } 
+    }
+    
+    displayFaceBox = (box) => {
+        this.setState({box: box})
     }
 
     onInputChange = (event) => {
@@ -29,7 +50,7 @@ class App extends Component {
     }
 
     onButtonSubmit = () => {
-        this.setState({IMAGE_URL: this.state.input})
+        this.setState({imageUrl: this.state.input});
         const raw = JSON.stringify({
             "user_app_id": {
                 "user_id": USER_ID,
@@ -39,28 +60,26 @@ class App extends Component {
               {
                 "data": {
                     "image": {
-                        "url": IMAGE_URL
+                        "url": this.state.input
                     }
                 }
               }]
-          });
-          console.log(raw);
-  
-  
-          const requestOptions = {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Key ' + PAT
-          },
-          body: raw
-          };            
-  
-          fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        });
+
+        const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+        };            
+
+        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
             .then(response => response.json())
-            .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+            .then(result => this.displayFaceBox(this.calculateFaceLocation(result.outputs[0].data.regions[0].region_info.bounding_box)))
             .catch(error => console.log('error', error));
-    }
+    };
 
   render() {
       return (
@@ -73,7 +92,7 @@ class App extends Component {
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}            
             />
-            <FaceRecognition IMAGE_URL={this.state.IMAGE_URL} />
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
           </div>
       );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({IMAGE_URL}) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return(
     <div className='center'>
       <div className='absolute mt2'>
-        <img alt='' src={IMAGE_URL} width='500px' height='auto' />
+        <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
+        <div className='bounding_box' style={{top: box.top_row, right: box.right_col, bottom: box.bottom_row, left: box.left_col}}></div>
       </div>
     </div>
   );
