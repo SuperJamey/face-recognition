550db3c9057f8a0538d6a0f759bb3225f5fdafac
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-02-22 11:31:41 -0600

    not sure what happened

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             route: 'signin',
             isSignedIn: false
         } 
-    }
+    }p
     
     calculateFaceLocation = (resp) => {
         console.log(resp);
